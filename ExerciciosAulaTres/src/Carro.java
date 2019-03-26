789aabb23b5e65c321575547f78e16bd49b12687
Cassius Ariovaldo Paim <cassius.paim@gmail.com>
2019-03-26 20:32:24 -0300

    Exercício Seis

--- ExerciciosAulaTres/src/Carro.java
+++ ExerciciosAulaTres/src/Carro.java
@@ -7,7 +7,7 @@ public class Carro {
 	
 	public String combustivel;
 	
-	public Double valor;
+	public double valor;
 	
 	public String fabricante;
 	
@@ -23,5 +23,96 @@ public class Carro {
 	public boolean automatico;
 	
 	public boolean temAirBag;
+	
+	public int portas;
+
+	public int quantidadePassageiros;
+
+	public String tipo;
+
+	public String motor;
+	
+	public final double VALOR_CARRO_LUXO = 100000;
+
+	public Carro(String placa, String modelo, String combustivel, double valor,
+			String fabricante, String chassi, int anoModelo, int anoFabricacao,
+			String potencia, boolean automatico, boolean temAirBag, int portas,
+			int quantidadePassageiros, String tipo, String motor) {
+		super();
+		
+		this.placa = placa;
+		this.modelo = modelo;
+		this.combustivel = combustivel;
+		this.valor = valor;
+		this.fabricante = fabricante;
+		this.chassi = chassi;
+		this.anoModelo = anoModelo;
+		this.anoFabricacao = anoFabricacao;
+		this.potencia = potencia;
+		this.automatico = automatico;
+		this.temAirBag = temAirBag;
+		this.portas = portas;
+		this.quantidadePassageiros = quantidadePassageiros;
+		this.tipo = tipo;
+		this.motor = motor;
+	}
+	
+	public String getDescricaoDoObjeto() {
+		return    placa 
+				+ modelo 
+				+ combustivel 
+				+ valor 
+				+ (automatico ? 
+						" é automático " 
+						: "não é automático")
+				+ portas
+				/* concatenar com o 
+				 * resto das propriedades */ ;
+	}
+	
+	public boolean ehDeLuxo() {
+		boolean ehDeLuxo = false;
+		if (valor > VALOR_CARRO_LUXO) {
+			ehDeLuxo = true;
+		}
+		
+		return ehDeLuxo;
+	}
+	
+	
+//	
+//	public Carro(String placaParam,
+//				 String modeloParam,
+//				 String combustivelParam,
+//				 double valorParam,
+//				 int portasParam,
+//				 int quantidadePassageirosParam,
+//				 String fabricanteParam,
+//				 String chassiParam,
+//				 int anoModeloParam,
+//				 int anoFabricacaoParam,
+//				 String potenciaParam,
+//				 boolean automaticoParam,
+//				 boolean temAirBagParam,
+//				 String tipoParam,
+//				 String motorParam) {
+//		
+//		placa = placaParam;
+//		modelo = modeloParam;
+//		combustivel = combustivelParam;
+//		valor = valorParam;
+//		portas = portasParam;
+//		quantidadePassageiros = quantidadePassageirosParam;
+//		fabricante = fabricanteParam;
+//		chassi = chassiParam;
+//		anoModelo = anoModeloParam;
+//		anoFabricacao = anoFabricacaoParam;
+//		potencia = potenciaParam;
+//		automatico = automaticoParam;
+//		temAirBag = temAirBagParam;
+//		tipo = tipoParam;
+//		motor = motorParam;
+//		
+//	}
 	
 }
---
+++ ExerciciosAulaTres/src/TesteInstanciaCarro.java
@@ -0,0 +1,57 @@
+
+public class TesteInstanciaCarro {
+
+	public static void main(String[] args) {
+		Carro carro1 = new Carro();
+		carro1.placa = "IXX9088";
+		carro1.modelo = "GOL 1.6";
+		carro1.combustivel = "gasolina e álcool";
+		carro1.valor = 17000;
+		carro1.portas = 4;
+		carro1.quantidadePassageiros = 5;
+		carro1.fabricante = "Volkswagen";
+		carro1.chassi = "IDOASJDO39084323";
+		carro1.anoModelo = 2003;
+		carro1.anoFabricacao = 2003;
+		carro1.potencia = "97hp";
+		carro1.automatico = false;
+		carro1.temAirBag = false;
+		carro1.tipo = "hatch";
+		carro1.motor = "1.6";
+		
+		/*
+		 Primeiro conjunto de valores:
+• placa: IXX9088;
+• modelo: GOL 1.6;
+• combustível: gasolina e álcool;
+• valor: 17000;
+• portas: 4;
+• quantidade pessoas suportadas: 5;
+• fabricante: Volkswagen;
+• chassi: IDOASJDO39084323;
+• ano do modelo: 2003;
+• ano da fabricação: 2003;
+• potência: 97hp;
+• automático: não;
+• tem airbag: não;
+• Tipo: hatch;
+• Motor: 1.6.
+b. Segundo conjunto de valores:
+• placa: ABC4339;
+• modelo: Volvo XC60 3.0 Dynamic 5p;
+• combustível: Flex;
+• valor: dezessete mil reais;
+• fabricante: Volvo;
+• portas: 5;
+• cor: prata;
+• chassi: REWRFFW34574323;
+• ano do modelo: 2011;
+• ano da fabricação: 2011;
+• potência: 304 horse power;
+• automático: sim;
+• tem airbag: sim;
+• quantidade de pessoas suportas: 6.
+		 */
+	}
+	
+}
