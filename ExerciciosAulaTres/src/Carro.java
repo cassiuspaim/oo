d84461fd3c9d198760af9168fcc97c2eb27fc524
Cassius Ariovaldo Paim <cassius.paim@gmail.com>
2019-03-26 19:28:35 -0300

    Exercício 1 - lista de exercícios

---
+++ ExerciciosAulaTres/src/Carro.java
@@ -0,0 +1,26 @@
+//Exercício 1
+public class Carro {
+	
+	public String placa;
+	
+	public String modelo;
+	
+	public String combustivel;
+	
+	public Double valor;
+	
+	public String fabricante;
+	
+	public String chassi;
+	
+	public int anoModelo;
+	
+	public int anoFabricacao;
+	
+	public String potencia;
+	
+	public boolean automatico;
+	
+	public boolean temAirBag;
+	
+}
